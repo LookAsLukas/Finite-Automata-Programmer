b5d784331a069e1b7c8074d2f176bd42c1ee1cc4
Nikolay Mashkin <n.mashkin@g.nsu.ru>
2025-10-02 10:11:44 +0700

    fixed builder, now definetly should work

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Building Automata Builder for Linux..."
+if [ ! -f ".venv/bin/activate" ]; then
+    rm -rf .venv/
+fi
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
 source .venv/bin/activate
-pip install -r requirements.txt
-pip install pyinstaller
-python build.py
+pip3 install -r requirements.txt
+pip3 install pyinstaller
+python3 build.py
